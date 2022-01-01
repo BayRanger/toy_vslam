b3982ae0de2fa6bb455201aabd799d5099ae65ef
Changxin He <c.he@campus.tu-berlin.de>
2022-01-01 16:03:13 +0100

    visualize the mp4

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # toy_vslam
+
+
+* link of test video: https://www.pexels.com/video/video-of-road-1666547/
--- slam.py
+++ slam.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
-
 import cv2
+#import sdl2.ext
+
+scale = 4
+H = 1080//scale
+W = 1920//scale
 
 def process_frame(img):
-  print("hello")
-  img = cv2.resize(img,(1080//4,1920//4))
-  print(img.shape)
+    #print("proces img")
+    img = cv2.resize(img,(W,H))
+    #events = sdl2.ext.get_events()
+    cv2.imshow("image",img)
+    #window.refresh()
+
 
 if __name__ =="__main__":
-  cap = cv2.VideoCapture("drive_view.mp4")
-  print("Hello World")
-  while cap.isOpened():
-    ret, frame = cap.read()
-    if ret  == True:
-      process_frame(frame)
-    else:
-      breakap = cv2.VideoCapture("drive_view.mp4")
-  print("Hello World")
-  while cap.isOpened():
-    ret, frame = cap.read()
-    if ret  == True:
-      process.frame(frame)
-    else:
-      break
+    cap = cv2.VideoCapture("test.mp4")
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+    while cap.isOpened():
+        ret,frame = cap.read()
+        if ret==True:
+            process_frame(frame)
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    
+
